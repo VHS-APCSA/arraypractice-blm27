f207cd7e3a26caae760d713bf764601373b5e4c4
dhmorton <davidm@VUSD.SolanoCOE.k12.ca.us>
2017-11-12 13:43:41 -0800

    Initial commit.

---
+++ ArrayPractice.java
@@ -0,0 +1,62 @@
+
+public class ArrayPractice 
+{
+	private int[] array;
+	public ArrayPractice()
+	{
+		//initialize an int[] array with 50 elements and fill in the array elements by calling initArray.
+		
+		initArray();
+	}
+	/**
+	 * This method should iterate over the array
+	 * and initialize each element of the array with a random int between 1 and 20 inclusive.
+	 */
+	public void initArray()
+	{
+		
+	}
+	/**
+	 * This method gets the value of an element in the array at a given index.
+	 * @param index The index of an element in the array.
+	 * @return The value of the element at the given index. 
+	 */
+	public int getByIndex(int index)
+	{
+		
+	}
+	/**
+	 * This method should iterate over the array and calculate the average value of the elements. 
+	 * @return The average value of the elements in the array.
+	 */
+	public double average()
+	{
+		
+	}
+	/**
+	 * This method should find the index of the maximum element in the array. 
+	 * @return The index of the maximum valued element in the array.
+	 * If there are multiple maximal values then the index of the first one should be returned.
+	 */
+	public int indexOfMaxValue()
+	{
+		
+	}
+	/**
+	 * This method should return an array that contains the indexes of all of the even array elements.
+	 * The array should only be as big as necessary to contain all of the even numbers.
+	 * @return An int[] array containing the indexes of the even valued elements.
+	 */
+	public int[] indexesOfEvens()
+	{
+		
+	}
+	/** This method should return a formatted String of the array data.
+	 * @return A nicely formatted String.
+	 */
+	@Override
+	public String toString()
+	{
+		
+	}
+}
